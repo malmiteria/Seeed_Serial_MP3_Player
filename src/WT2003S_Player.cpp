1239902539878ace47c9f095437309216c7b0cc4
malmiteria <malmiteria@gmail.com>
2023-01-30 16:46:29 +0100

    .

--- src/WT2003S_Player.cpp
+++ src/WT2003S_Player.cpp
@@ -82,7 +82,7 @@ again:
         Serial.println(time);
         Serial.println(_serial->available());
         while ((millis() - time) < WT2003S_TIMEOUT) {
-            if (1) {
+            if (_serial->available()) {
                 Serial.println("serial available");
                 if (commandBytes[0] == _serial->read()) {
                     Serial.println("byte 0 is what we read");
@@ -90,6 +90,7 @@ again:
                     break;
                 }
             }
+            is_again = true;
         }
         if (is_again) {
             if (++again_count == 5) {
